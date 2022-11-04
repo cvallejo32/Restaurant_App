26ae37e739bed92591560f764f72738100180a74
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-11-04 15:36:26 -0600

    primeras paginas y componentes creados

---
+++ restaurant_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutPage from './pages/AboutPage';
+import HomePages from './pages/HomePages';
+
+const App = () => {
+  return (
+    <>
+    <HomePages/>
+    <AboutPage/>
+    </>
+  )
+}
+
+export default App
---
+++ restaurant_app/src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Title from '../components/Title';
+import Footer from '../components/Footer';
+
+
+const AboutPage = () => {
+  return (
+    <>
+      <NavBar />
+      <Title />
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
