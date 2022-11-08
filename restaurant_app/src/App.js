27a2df8cee626bb68b991b46d7ff1f6e34ecdd35
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-11-07 22:50:41 -0600

    Added Userlist and listpage

--- restaurant_app/src/App.js
+++ restaurant_app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ListPage from './pages/ListPage';
 import AboutPage from './pages/AboutPage';
 import HomePages from './pages/HomePages';
 
 const App = () => {
   return (
     <>
-    <HomePages/>
-    <AboutPage/>
+    <HomePages />
+    <AboutPage />
+    <ListPage />
     </>
   )
 }
