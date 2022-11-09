09fb91506bc9fb6d14516361d4da1a7c64a1c5f6
cvallejo32 <crvallejo.dwfc6@ucamp.utel.edu.mx>
2022-11-09 14:06:13 -0600

    Added bootstrap

--- restaurant_app/src/App.js
+++ restaurant_app/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+
 import ListPage from './pages/ListPage';
 import AboutPage from './pages/AboutPage';
 import HomePages from './pages/HomePages';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 const App = () => {
   return (
